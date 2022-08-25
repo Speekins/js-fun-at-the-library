d543406aa4e043e320bd9c8e7a778690f0bcded0
Spencer Haka <spencer.haka@gmail.com>
2022-08-25 10:33:26 -0600

    complete all shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,10 +1,32 @@
 function shelfBook(title, shelf) {
+  if (shelf.length === 3) {
+    return;
+  }
   shelf.unshift(title);
+}
+
+function unshelfBook(title, shelf) {
+  let position;
+  for (let idx = 0; idx < shelf.length; idx++) {
+    if (shelf[idx].title === title) {
+      position = idx;
+    }
+  }
+  shelf.splice(position, 1);
+}
+
+function listTitles(shelf) {
+  return shelf.map(object => object.title).join(', ');
+}
+
+function searchShelf(shelf, name) {
+let names = shelf.map(book => book.title);
+return names.includes(name);
 }
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
