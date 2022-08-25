7559b075707360bdeb464ae9d04d0f81b29fce31
Spencer Haka <spencer.haka@gmail.com>
2022-08-24 20:02:33 -0600

    complete book test suite

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(charName, charAge, charPronouns) {
+  return {
+    name: charName,
+    age: charAge,
+    pronouns: charPronouns
+  }
+}
+
+function saveReview(review, array) {
+
+  if (!array.includes(review)) {
+    array.push(review);
+  }
+//array = [...new Set(array)]; This was a cool solution, but it wasn't passing the tests!
+}
+
+function calculatePageCount(title) {
+  return title.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre,
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
