04c97bc871c138b3e8b818eab2386b80cdd6d678
Spencer Haka <spencer.haka@gmail.com>
2022-08-25 22:01:55 -0600

    finish library test suite cases

--- src/Library.js
+++ src/Library.js
@@ -10,12 +10,23 @@ function createLibrary(name) {
 }
 
 function addBook(library, book) {
-  let genre = book.genre
+  var genre = book.genre
   library.shelves[genre].push(book);
+}
+
+function checkoutBook(library, title, genre) {
+  var shelf = library.shelves[genre];
+  for (var idx = 0; idx < shelf.length; idx++) {
+    if (shelf[idx].title === title) {
+      shelf.splice(idx, 1);
+      return `You have now checked out ${title} from the ${library.name}`
+    }
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
 }
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
