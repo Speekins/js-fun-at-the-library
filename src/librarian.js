3bb753fc7d7160a895890368bc92b4ba35fa521e
Spencer Haka <spencer.haka@gmail.com>
2022-08-28 13:19:49 -0600

    refactor findBook function to utilize helper function

--- src/librarian.js
+++ src/librarian.js
@@ -11,26 +11,43 @@ class Librarian {
     return `Hello, ${name}!`;
   }
 
-  findBook(title) {
-    let fantasy = this.library.shelves.fantasy;
-    let fiction = this.library.shelves.fiction;
-    let nonFiction = this.library.shelves.nonFiction;
+  // findBook(title) {
+  //   let fantasy = this.library.shelves.fantasy;
+  //   let fiction = this.library.shelves.fiction;
+  //   let nonFiction = this.library.shelves.nonFiction;
 
-    for (var idx = 0; idx < fantasy.length; idx++) {
-      if (fantasy[idx].title === title){
-        fantasy.splice(idx, 1);
-        return `Yes, we have ${title}`
-      }
-    }
-    for (var idx = 0; idx < fiction.length; idx++) {
-      if (fiction[idx].title === title){
-        fiction.splice(idx, 1);
-        return `Yes, we have ${title}`
-      }
-    }
-    for (var idx = 0; idx < nonFiction.length; idx++) {
-      if (nonFiction[idx].title === title){
-        nonFiction.splice(idx, 1);
+  //   for (var idx = 0; idx < fantasy.length; idx++) {
+  //     if (fantasy[idx].title === title){
+  //       fantasy.splice(idx, 1);
+  //       return `Yes, we have ${title}`
+  //     }
+  //   }
+  //   for (var idx = 0; idx < fiction.length; idx++) {
+  //     if (fiction[idx].title === title){
+  //       fiction.splice(idx, 1);
+  //       return `Yes, we have ${title}`
+  //     }
+  //   }
+  //   for (var idx = 0; idx < nonFiction.length; idx++) {
+  //     if (nonFiction[idx].title === title){
+  //       nonFiction.splice(idx, 1);
+  //       return `Yes, we have ${title}`
+  //     }
+  //   }
+
+  //   return `Sorry, we do not have ${title}`
+  // }
+
+  findBook(title) {
+    var shelves = 
+    this.library.shelves.fantasy
+    .concat(this.library.shelves.fiction, this.library.shelves.nonFiction);
+    var genreShelf;
+
+    for (var idx = 0; idx < shelves.length; idx++) {
+      if (shelves[idx].title === title) {
+        genreShelf = shelves[idx].genre;
+        unshelfBook(this.library.shelves, title, genreShelf);
         return `Yes, we have ${title}`
       }
     }
@@ -41,6 +58,16 @@ class Librarian {
   calculateLateFee(daysLate) {
     return Math.ceil(daysLate * .25);
   }
+}
+
+function unshelfBook(library, title, shelf) {
+  let position;
+  for (let idx = 0; idx < library[shelf].length; idx++) {
+    if (library[shelf][idx].title === title) {
+      position = idx;
+    }
+  }
+  library[shelf].splice(position, 1);
 }
 
 module.exports = Librarian;
